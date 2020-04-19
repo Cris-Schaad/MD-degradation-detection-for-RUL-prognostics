Corrected MD calculation

data/FEMTO/FEMTO_data_processing.py
@@ -18,6 +18,9 @@ sample_rul = []
 for i in range(len(data_raw)):
     sample = data_raw[i]
     print(sample_name[i])       
+
+    x_vib = sample[:,0]
+    y_vib = sample[:,1]
     
     #Spectograms
     sampling = 25600        #hertz            
@@ -26,18 +29,20 @@ for i in range(len(data_raw)):
     win_len_time_step = 2560//win_len
     spectogram_pixel_overlap = 0
 
-    subsample_stft = stft(sample, nperseg=win_len, noverlap=spectogram_pixel_overlap, fs=sampling)
+    subsample_stft = stft(x_vib, nperseg=win_len, noverlap=spectogram_pixel_overlap, fs=sampling)
     subsamples_spectograms = np.absolute(subsample_stft[2])
     sample_spectogram.append(np.transpose(subsamples_spectograms))    
     
     #Features
-    feature_win_len = 10             #miliseconds
+    feature_win_len = 100             #miliseconds
     feature_win_len = int(feature_win_len*sampling//1000)
     feature_win_len_time_step = 2560//win_len
     
-    subsamples = aux.windows(sample, feature_win_len, feature_win_len)        
-    subsamples_features = np.asarray([aux.features(i) for i in subsamples])
-    sample_features.append(subsamples_features)
+    x_vib_subsamples = aux.windows(x_vib, feature_win_len, feature_win_len)        
+    y_vib_subsamples = aux.windows(y_vib, feature_win_len, feature_win_len)        
+    x_vib_subsamples_features = np.asarray([aux.features(i) for i in x_vib_subsamples])
+    y_vib_subsamples_features = np.asarray([aux.features(i) for i in y_vib_subsamples])    
+    sample_features.append(np.column_stack((x_vib_subsamples_features,y_vib_subsamples_features)))
 
     #RUL
     rul = subsample_stft[1]*1000

data/FEMTO/FEMTO_raw_to_npz.py
@@ -16,7 +16,7 @@ raw_samples = []
 sample_name = []
 samples_rul = []
 for sample in datasets:
-    print('Analizando: ', sample)
+    print('Processing: ', sample)
     data_files = os.listdir(os.path.join(folder, sample))
     vib_files = np.asarray([i for i in data_files if 'acc' in i])
     vib_files.sort()
@@ -24,19 +24,19 @@ for sample in datasets:
     sample_data = []    
     for file in vib_files:
         dataframe = pd.read_csv(os.path.join(folder, sample, file), header=None)
-        data_sample = np.asarray(dataframe.iloc[:,4])
+        data_segment = np.asarray(dataframe.iloc[:,4:])
         
-#            if np.max(np.abs(data_sample)) >= 20:
+#            if np.max(np.abs(data_segment)) >= 20:
 #                if np.max(np.abs(sample_data[-1])) >= 20:
 #                    if np.max(np.abs(sample_data[-2])) >= 20:
 #                        break
-        sample_data.append(data_sample)
+        sample_data.append(data_segment)
         
     sample_times = np.asarray([i/25600 for i in range(2560)])
     times = np.asarray([10*i for i in range(len(sample_data))])
     rul = np.asarray([sample_times+i for i in times]).flatten()[::-1]
     
-    raw_samples.append(np.asarray(sample_data).flatten())
+    raw_samples.append(np.concatenate(sample_data))
     samples_rul.append(rul)
     sample_name.append(sample)
         

data/MD_detection_FEMTO.py
@@ -20,12 +20,17 @@ feature_timestep_per_image = data_set['feature_timestep']
 dataset_names = data_set['name']
 
 
+def cum_mean(x, time_window):
+    return np.asarray([np.mean(x[:i]) for i in range(time_window, len(x))])
+
+
 time_window = 6
-healthy_cycles = 6*feature_timestep_per_image*60
+healthy_cycles = 6*30
     
 x_data_healthy = np.asarray([i[:healthy_cycles] for i in x_data])    
-m_d = MD.MahalanobisDistance(mode='scipy')
+m_d = MD.MahalanobisDistance(mode='covariance')
 ms = m_d.fit_predict(np.concatenate(x_data_healthy))
+print('Mean MD in MS: {:.2f}'.format(np.mean(ms)))
 
 x_train_md = np.asarray([m_d.fit(i) for i in x_data])
     
@@ -34,46 +39,50 @@ x_train_md = np.asarray([m_d.fit(i) for i in x_data])
 #     plt.plot(i)
     
 #Degradation detector
-k = 12; n = 18
+k = 2; n = 3
 threshold = np.mean(ms) + 2*np.std(ms)
-print('Threshold: {:.2f}'.format(threshold))
 detector = dd.Detector(k, n, threshold = threshold)
 
 # Training set
-deg_ind = []
+deg_ind = []; end_md = []
 for sample in x_data:
+    # md_sample = cum_mean(m_d.fit(sample), 6*feature_timestep_per_image)
     md_sample = m_d.fit(sample)
     test_deg = detector.detect(md_sample, prints=True)
         
     print('Test sample RUL: {:.0f}'.format(len(md_sample)-1-test_deg))
     deg_ind.append(test_deg/feature_timestep_per_image)
+    end_md.append(md_sample[-1])
     
     plt.figure()
     plt.plot(md_sample)
     plt.axhline(threshold, c='C1')
     plt.axvline(test_deg, c='r')
+    
+plt.figure()
+plt.hist(end_md, 10)
  
 
-# Data to images
-del x_data; x_data = data_set['data_spectograms']  
-x_data, y_data = dp.time_window_sampling(x_data, y_data, timestep_per_image, 
-                                        time_step=timestep_per_image, 
-                                        add_last_dim=True, temporal_axis=1)
-x_data = dp.samples_reverse(x_data)
+# # Data to images
+# del x_data; x_data = data_set['data_spectograms']  
+# x_data, y_data = dp.time_window_sampling(x_data, y_data, timestep_per_image, 
+#                                         time_step=timestep_per_image, 
+#                                         add_last_dim=True, temporal_axis=1)
+# x_data = dp.samples_reverse(x_data)
 
 
-# Sampling from degradation start index
-y_data = np.asarray([np.expand_dims(10*np.linspace(len(i), 1, len(i)), axis=1) for i in x_data])
-x_data, y_data = detector.sampling_from_index(x_data, y_data, deg_ind, time_window)
+# # Sampling from degradation start index
+# y_data = np.asarray([np.expand_dims(10*np.linspace(len(i), 1, len(i)), axis=1) for i in x_data])
+# x_data, y_data = detector.sampling_from_index(x_data, y_data, deg_ind, time_window)
 
 
-# Sequence of images
-x_data, y_data = dp.time_window_sampling(x_data, y_data, time_window)
+# # Sequence of images
+# x_data, y_data = dp.time_window_sampling(x_data, y_data, time_window)
 
 
-# Saving
-np.savez(os.path.join(data_dir, 'FEMTO_dataset.npz'),
-        x_data = x_data,
-        y_data = y_data,
-        data_names = dataset_names,
-        allow_pickle=True)
+# # Saving
+# np.savez(os.path.join(data_dir, 'FEMTO_dataset.npz'),
+#         x_data = x_data,
+#         y_data = y_data,
+#         data_names = dataset_names,
+#         allow_pickle=True)

data/MahalanobisDistance.py
@@ -7,69 +7,70 @@ class MahalanobisDistance():
     def __init__(self, feature_axis=0, mode='inv'):
         self.mode = mode
         self.axis = feature_axis
+        self.ms = False
 
-
+    
     def fit_predict(self, x):
-        md, u, s, cov = self.mahalanobis_distance(x, axis=self.axis)
+        """ Defines a Mahalanobis space"""
+        
+        md, u, s, cov = self.mahalanobis_distance(x)
         self.var_mean = u
         self.var_std = s
         self.cov = cov
+        self.ms = True
+        
         return md
         
     
     def fit(self, x):    
-        md, _, _, _ = self.mahalanobis_distance(x, mean=self.var_mean, std=self.var_std, cov=self.cov)
-        return md
+        """ Calculates distance to the defined Mahalanobis space"""
+        
+        if self.ms:
+            md, _, _, _ = self.mahalanobis_distance(x, mean=self.var_mean, 
+                                                    std=self.var_std, 
+                                                    cov=self.cov)
+            return md
+        else:
+            print('Mahalanobis space not constructed')
+            
     
-    
-    def mahalanobis_distance(self, x, mean=None, std=None, cov=None, axis=0):
+    def mahalanobis_distance(self, x, mean=None, std=None, cov=None):
         
         n_samples = x.shape[0]
         n_vars = x.shape[1]
         
         if mean is None:
-            mean = np.mean(x, axis=axis)
+            mean = np.mean(x, axis=self.axis)
         if std is None:
-            std = np.std(x, axis=axis)
-        
-        z = np.ones_like(x)
-        for i in range(n_vars):
-            z[:,i] = (x[:,i]-mean[i])/std[i]
+            std = np.std(x, axis=self.axis)
             
-        if self.mode == 'scipy':
+            
+        if self.mode == 'covariance':
             if cov is None:    
-                cov = np.cov(x, rowvar=False)         
+                cov = self.covariance_matrix(x)  
 
             md = np.zeros(n_samples)    
-            for i in range(len(z)):
-                x_i = x[i,:]                
-                md[i] = mahalanobis(x_i, mean, np.linalg.inv(cov))/n_vars
+            for i in range(len(x)):
+                x_i = x[i,:] - mean
+
+                MD = np.matmul(np.linalg.inv(cov), x_i)
+                md[i] = np.sqrt(np.dot(np.transpose(x_i), MD)/(n_vars-1))
+                
     
-        if self.mode == 'inv':
+        if self.mode == 'correlation':
+            z = np.ones_like(x)
+            for i in range(n_vars):
+                z[:,i] = (x[:,i]-mean[i])/std[i]
+                
             if cov is None:    
-                cov = self.correlation_matrix(z)           
+                cov = self.correlation_matrix(z)       
 
             md = np.zeros(n_samples)    
             for i in range(len(z)):
                 z_i = z[i,:]
                 
                 MD = np.matmul(np.linalg.inv(cov), z_i)
-                md[i] = np.dot(np.transpose(z_i), MD)/n_vars
-    
-        if self.mode == 'GS':
-            U = np.zeros_like(z)
-            for i in range(n_vars):
-                if i == 0:
-                    U[:,i] = z[:,i]
-                else:
-                    proyections = [U[:,k]*np.dot(z[:,i],U[:,k])/np.dot(U[:,k], U[:,k]) for k in range(i)]
-                    U[:,i] = z[:,i] - np.sum(proyections, axis=0)
-           
-            s = np.std(U, axis=axis)
-            md = np.zeros(n_samples)    
-            for i in range(len(z)):
-                md[i] = np.mean([np.power(U[i,k]/s[k], 2) for k in range(n_vars)])
-            
+                md[i] = np.sqrt(np.dot(np.transpose(z_i), MD)/(n_vars-1))
         return md, mean, std, cov
             
                 
@@ -91,6 +92,13 @@ class MahalanobisDistance():
                 cov[i,j] = sup_sum/np.sqrt(i_sum*j_sum)
         return cov
     
-    
-    def mahalanobis_space(self):
-        return self.ms
+
+    def covariance_matrix(self, x):
+        n_vars = x.shape[1]
+        cov = np.zeros((n_vars, n_vars))
+        for i in range(n_vars):
+            for j in range(n_vars):
+                x_i = x[:,i] - np.mean(x[:,i])
+                x_j = x[:,j] - np.mean(x[:,j])
+                cov[i,j] = np.mean(x_i*x_j)
+        return cov